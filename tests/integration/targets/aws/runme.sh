1811568f0e14bdca1f5b0e9478eff182ac0e7083
Alina Buzachis <abuzachis@redhat.com>
2023-01-30 16:43:09 +0100

    Rename resources
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- tests/integration/targets/aws/runme.sh
+++ tests/integration/targets/aws/runme.sh
@@ -4,9 +4,10 @@ set -eux
 
 export ANSIBLE_CALLBACKS_ENABLED="pravic.pravic.state"
 
-STATE_FILE="state.json"
-trap 'rm -rvf "${STATE_FILE}"' EXIT
+STATE_FILE_1="state_1.json"
+STATE_FILE_2="state_2.json"
+trap 'rm -rvf "${STATE_FILE_1}" "${STATE_FILE_2}"' EXIT
 
-ansible-playbook playbooks/check_mode.yml  -i localhost -e state_file=STATE_FILE "$@"
+ansible-playbook playbooks/check_mode.yml  -i localhost -e state_file=STATE_FILE_1 "$@"
 
-ansible-playbook playbooks/resource_dependency.yml -i localhost -e state_file=STATE_FILE "$@"
+ansible-playbook playbooks/resource_dependency.yml -i localhost -e state_file=STATE_FILE_2 "$@"
